4db5a7868c183da72fa48384fc8ac26b7f40e775
eric Cardoso <eric.alves.cardoso82@gmail.com>
2022-05-10 15:14:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Crie uma variável que receba um nome;
+let nome = "Eric"
+console.log(nome)
+// Crie uma variável que receba um número;
+let numero = 26
+console.log(numero)
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let anoAtual = 2022
+console.log(numero - anoAtual)
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log(numero + anoAtual)
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Estou mexendo no console.log")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = true
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(quartaFeira)
