e7707bcbb5b32423533170b68c26c0684027d4bf
eric Cardoso <eric.alves.cardoso82@gmail.com>
2022-05-11 20:16:48 -0300

    segunda atividade

---
+++ Atividade dia 11/script.js
@@ -0,0 +1,44 @@
+//Dica rápida, frases (string) devem ser colocadas entre aspas (simples ou duplas) e números não levam aspas.
+
+//Exiba suas respostas no console e atribua o tipo delas também
+
+// Declarar uma variável com o seu nome 
+const nome = "Eric"
+console.log(typeof nome, nome)
+// Declarar uma variável com a sua idade 
+const idade = 26
+console.log(typeof idade, idade)
+// Declarar uma variável com a sua comida favorita e não atribuir valor 
+let comidaFavorita
+console.log(typeof comidaFavorita)
+// Atribuir valor a sua comida favorita 
+const comidafavorita = "Cozido"
+console.log(typeof comidafavorita, comidafavorita)
+// Declarar 5 variáveis diferentes sem valores
+let primeira
+let segunda
+let terceira
+let quarta
+let quinta
+console.log(typeof primeira, segunda,terceira,quarta,quinta)
+// Declare uma variável contendo uma string 
+const string = "Olá"
+console.log(typeof string, string)
+// Declare uma variável contendo um número dentro de uma string 
+const numeroString = "Senha123"
+console.log(typeof numeroString, numeroString)
+// Declare uma variável com a sua idade 
+const idade2 = 15
+c
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas 
+const nome1 = "Eric "
+const sobrenome = "Alves"
+
+console.log(typeof nome1, typeof sobrenome, nome1 + sobrenome)
+// Coloque a seguinte frase em uma variável: It's time 
+const frase = 'It\'\s time'
+
+console.log(typeof frase, frase)
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome)
+
